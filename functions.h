b431837362cbc5f0f264dbd04f334055a225dab8
artemgon <artemhoncharuk123@gmail.com>
2024-10-29 14:28:31 +0200

    Task 3 is almost completed. There some issues.

--- functions.h
+++ functions.h
@@ -1,5 +1,7 @@
 #include "lib.h" 
 
+void randomArr(int* ptr, int size);
 
+void printArr(int* ptr, int size);
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+	srand(time(NULL));
+	const long size = 7;
+	int array_1[size], array_2[size], * ptr_1 = array_1, * ptr_2 = array_2;
+	randomArr(array_1, size);
+	printArr(array_1, size);
+	for (int i = 0, j = size - 1; i < size; ++i, --j)
+	{
+		*(ptr_2 - j) = *(ptr_1 + i);
+	}
+	printArr(array_2, size);
 	return 0;
 }
