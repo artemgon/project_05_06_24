27b3e51bd03ca5890d3d7efd50e82fb7bc9a4d68
artemgon <artemhoncharuk123@gmail.com>
2024-10-29 12:43:10 +0200

    Task 5 is created.

--- functions.h
+++ functions.h
@@ -1,5 +1,14 @@
 #include "lib.h" 
 
-
+template<typename T>
+T sumFunc(T* ptr, int size)
+{
+	T sum = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		sum += *(ptr + i);
+ 	}
+	return sum;
+}
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+	srand(time(NULL));
+	const long SIZE = 10;
+	int array[SIZE], *ptr = array;
+	for (int i = 0; i < SIZE; ++i) {
+		*(ptr + i) = rand() % 100;
+	}
+	for (int i = 0; i < SIZE; ++i) {
+		cout << *ptr << " ";
+		++ptr;
+	}
+	cout << endl;
+	int result = sumFunc(array, SIZE);
+	cout << result;
 	return 0;
 }
