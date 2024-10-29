0feca3e7a811ddbf5d927bf2bd7830ddc0d0f55e
artemgon <artemhoncharuk123@gmail.com>
2024-10-29 12:16:03 +0200

    Task 4 is completd.

--- functions.h
+++ functions.h
@@ -1,5 +1,57 @@
 #include "lib.h" 
 
+template<typename T>
+T addition(int quantity, T a, ...)
+{
+	va_list va;
+	va_start(va, a);
+	T sum = a;
+	for (int i = 1; i < quantity; ++i) {
+		T temp = va_arg(va, T);
+		sum += temp;
+	}
+	va_end(va);
+	return sum;
+}
 
+template<typename T>
+T subtraction(int quantity, T a, ...)
+{
+	va_list va;
+	va_start(va, a);
+	T diff = a;
+	for (int i = 1; i < quantity; ++i) {
+		T temp = va_arg(va, T);
+		diff -= temp;
+	}
+	va_end(va);
+	return diff;
+}
 
+template<typename T>
+T multiplication(int quantity, T a, ...)
+{
+	va_list va;
+	va_start(va, a);
+	T product = a;
+	for (int i = 1; i < quantity; ++i) {
+		T temp = va_arg(va, T);
+		product *= temp;
+	}
+	va_end(va);
+	return product;
+}
 
+template<typename T>
+T division(int quantity, T a, ...)
+{
+	va_list va;
+	va_start(va, a);
+	T quotient = a;
+	for (int i = 1; i < quantity; ++i) {
+		T temp = va_arg(va, T);
+		quotient /= temp;
+	}
+	va_end(va);
+	return quotient;
+}
--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+	int a = 0, a_1 = 1.0, b = -33, c = -1, d = 4, e = 9, f = 10, g = -8, h = 10.0, j = 2.0;
+	int result_1 = addition(7, a, b, c, d, e, f, g);
+	int result_2 = subtraction(2, a, h);
+	cout << result_1 - result_2 << endl;
+	int result_3 = multiplication(4, a_1, d, e, h);
+	float result_4 = division(2, h, j);
+	cout << result_3 << endl;
+	cout << result_4 << endl;
 	return 0;
 }
