29de00b8459c49e6ceec0663756d078af6a83d24
artemgon <artemhoncharuk123@gmail.com>
2024-10-29 14:24:00 +0200

    Task 2 is almost completed, some fixes required.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,29 @@
 
 int main()
 {
+	srand(time(NULL));
+	const int size = 7;
+	//first variant
+	int array[size], *ptr = array, temp = 0;
+	randomArr(array, size);
+	printArr(array, size);
+	for (int i = 0; i < size / 2; ++i)
+	{
+		temp = array[size - i - 1];
+		array[size - i - 1] = *(ptr + i);
+		*(ptr + i) = temp;
+	}
+	printArr(array, size);
+	//second variant
+	/*int array_1[size], array_2[size], * ptr_1 = array_1, *ptr_2 = &array_2[size-1];
+	randomArr(array_1, size);
+	printArr(array_1, size);
+	for (int i = 0; i < size; ++i) {
+		*(ptr_2 + (size - 1 - i)) = *(ptr_1 + i);
+	}
+	for (int i = 0; i < size; ++i) {
+		*(ptr_1 + i) = *(ptr_2 + i);
+	}
+	printArr(array_1, size);*/
 	return 0;
 }
